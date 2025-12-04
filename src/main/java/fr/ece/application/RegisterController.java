46d2386f4710f79d7f71fef70187dded2a8211c5
Hocine661 <azouaouhocine66@gmail.com>
2025-12-04 09:04:18 +0100

    "Mise à jour"

--- src/main/java/fr/ece/application/RegisterController.java
+++ src/main/java/fr/ece/application/RegisterController.java
@@ -1,16 +1,30 @@
 package fr.ece.application;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
-public class RegisterController extends Application {
+public class RegisterController {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    @FXML
+    private TextField nomField;
 
-    @Override
-    public void start(Stage primaryStage) {
+    @FXML
+    private TextField prenomField;
 
-    }
+    @FXML
+    private TextField emailField;
+    @FXML
+    private PasswordField mdpField;
+
+    @FXML
+    private RadioButton eleveRadio;
+
+    @FXML
+    private RadioButton ScolaritéRadio;
+
+    @FXML
+    private Button inscriptionButton;
 }

6bb2859a7114bcf840f8ab0bdeda2497ab9a0224
Edouard232005 <Edouard.aer@gmail.com>
2025-12-04 09:44:49 +0100

    add page register

---
+++ src/main/java/fr/ece/application/RegisterApplication.java
@@ -0,0 +1,26 @@
+package fr.ece.application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RegisterApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("register.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Création de compte");
+        primaryStage.show();
+
+    }
+}

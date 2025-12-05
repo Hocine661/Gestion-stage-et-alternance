af5ddfd09f9497664e6274856ad26e5c55c6afb3
Hocine661 <azouaouhocine66@gmail.com>
2025-12-05 16:20:49 +0100

    "Ajout de la méthode inscription pour pouvoir effectuer les inscriptions"

--- src/main/java/fr/ece/application/RegisterController.java
+++ src/main/java/fr/ece/application/RegisterController.java
@@ -1,10 +1,7 @@
 package fr.ece.application;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class RegisterController {
 
@@ -23,8 +20,47 @@ public class RegisterController {
     private RadioButton eleveRadio;
 
     @FXML
-    private RadioButton ScolaritéRadio;
+    private RadioButton scolaritéRadio;
 
     @FXML
     private Button inscriptionButton;
+
+    @FXML
+    private ToggleGroup roleGroup;
+
+
+
+
+
+    @FXML
+    private void inscription() {
+        String nom = nomField.getText();
+        String prenom = prenomField.getText();
+        String email = emailField.getText();
+        String motDePasse = mdpField.getText();
+
+        // choix du role
+        RadioButton selectedRadio = (RadioButton) roleGroup.getSelectedToggle();
+        String role = selectedRadio != null ?
+                (selectedRadio.getText().equalsIgnoreCase("Eleve") ? "eleve" : "admin") :
+                null;
+
+
+        if (nom.isEmpty() || prenom.isEmpty() || email.isEmpty() || motDePasse.isEmpty() || role == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Veuillez remplir tous les champs et choisir un rôle.");
+            alert.showAndWait();
+            return;
+        }
+
+        //En attent de la création d'un package pour gerer la connexion, les mots de passes.....
+        boolean success = .registerUser(nom, prenom, email, motDePasse, role);
+
+        if (success) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Inscription réussie !");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Erreur lors de l'inscription. Verifier votre email.");
+            alert.showAndWait();
+        }
+    }
 }

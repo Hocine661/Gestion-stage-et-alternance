a5637923da25b06aad286485d5bde3ccd7b32b40
Acuche Yehouessi <sidney.acucheyehouessi@edu.ece.fr>
2025-12-08 08:15:47 +0100

    update

---
+++ src/main/java/fr/ece/application/DocumentController.java
@@ -0,0 +1,16 @@
+package fr.ece.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DocumentController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
